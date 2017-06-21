3e4877373a53c314759aa3117bfd746608d6919c
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2017-06-21 12:48:31 -0400

    fixes open-order seed script.

--- orders/seed/open-order.sh
+++ orders/seed/open-order.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 curl http://localhost:8082/orders/commands/open \
-  --data '{"customer": "http://localhost:9002/customers/1740f165-2d30-9670-5ef0-8a0a3c0fe78f"}' \
+  --data '{"customer": "http://localhost:8081/customers/1740f165-2d30-9670-5ef0-8a0a3c0fe78f"}' \
   -H 'Content-Type:application/json'
 echo
